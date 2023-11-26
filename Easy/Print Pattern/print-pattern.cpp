4fe9b27ebcdc6bdfd10f5f8e3db1a2b13dc0b963
Smit R. PATEL <smitrpatel19@gnu.ac.in>
2023-11-26 13:28:21 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Print Pattern/print-pattern.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    void f(int n,vector<int>&v){
+        v.push_back(n);
+        if(n>0) f(n-5,v);
+        if(n>0) v.push_back(n);
+    }
+    vector<int> pattern(int N){
+        vector<int>v;
+        f(N,v);
+        return v;
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        
+        Solution ob;
+        vector<int> ans = ob.pattern(N);
+        for(int u: ans)
+            cout<<u<<" ";
+        cout<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
