dfcebc401993ec8e237e0d7b2ef557b27bfa0abd
Smit R. PATEL <smitrpatel19@gnu.ac.in>
2024-06-19 00:00:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Number of Rectangles in a Circle/number-of-rectangles-in-a-circle.cpp
+++ Difficulty: Medium/Number of Rectangles in a Circle/number-of-rectangles-in-a-circle.cpp
@@ -12,12 +12,25 @@ class Solution {
   public:
     int rectanglesInCircle(int r) {
         // code here
-        int ans = 0;
-        for(int i = 1; i<2*r; ++i)ans += sqrt(4*r*r-i*i);
-        return ans;
-
+        int cnt = 0;
+        for(int i = 1; i <= 2*r; i++) {
+            int lo = 1, hi = 2*r;
+            int ans = 0;
+            while( lo <= hi) {
+                int mid = (lo + hi) / 2;
+                if( mid*mid + i * i <= 4*r*r) {
+                    ans = mid;
+                    lo = mid + 1;
+                } 
+                else hi = mid - 1;
+            }
+            cnt += ans;
+        }
+        return cnt;
     }
 };
+
+
 
 //{ Driver Code Starts.
 int main() {
