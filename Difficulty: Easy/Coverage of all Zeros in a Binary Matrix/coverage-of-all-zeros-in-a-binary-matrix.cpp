a861350632be9b99c0820f76985110a2c316b9be
Smit R. PATEL <smitrpatel19@gnu.ac.in>
2024-06-26 11:29:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Coverage of all Zeros in a Binary Matrix/coverage-of-all-zeros-in-a-binary-matrix.cpp
+++ Difficulty: Easy/Coverage of all Zeros in a Binary Matrix/coverage-of-all-zeros-in-a-binary-matrix.cpp
@@ -1,65 +1,57 @@
 //{ Driver Code Starts
 
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
 
 // } Driver Code Ends
 
 
 class Solution {
-public:
-
- int FindCoverage(vector<vector<int>>&v){
-        int res=0; 
-    
-        for(int i=0;i<v.size();i++){
-            
-            for(int j=0;j<v[0].size();j++){
-                
-                if(v[i][j]==0){
-                    
-                    //first  row
-                if(i!=0 && v[i-1][j]==1)
-                    res++;
-                  
-                //last row
-                if(i != v.size()-1 && v[i+1][j]==1)
-                    res++;
-                
-                //first col
-                if(j !=0 && v[i][j-1]==1)
-                    res++;
-                    
-                //last col
-                if(j != v[0].size()-1 && v[i][j+1]==1)
-                    res++;
-                }       
-            
+  public:
+    int findCoverage(vector<vector<int>>& matrix) {
+        // Code here
+        std::vector<int> rowDir = {-1, 1, 0, 0};
+        std::vector<int> colDir = {0, 0, -1, 1};
+        int coverage=0;
+        for(int i=0;i<matrix.size();i++)
+        {
+            for(int j=0;j<matrix[0].size();j++)
+            {
+                for(int k=0;k<4;k++)
+                {
+                    if(matrix[i][j]==0)
+                    {
+                       int newrow=i+rowDir[k];
+                       int newcol=j+colDir[k];
+                       if(newrow>=0 && newrow<matrix.size() && newcol >=0 && newcol<matrix[0].size() && matrix[newrow][newcol]==1)
+                       coverage++;
+                    }
+                }
+            }
         }
-        
-        }
-        return res;
+        return coverage;
     }
- 
+
 };
 
 
 //{ Driver Code Starts.
-int main(){
-	int tc;
-	cin >> tc;
-	while(tc--){
-		int n, m;
-		cin >> n >> m;
-		vector<vector<int>>matrix(n, vector<int>(m, 0));
-		for(int i = 0; i < n; i++)
-			for(int j = 0; j < m; j++)
-				cin >> matrix[i][j];
-		Solution obj;
-		int ans = obj.FindCoverage(matrix);
-		cout << ans << "\n";
-	}
-	return 0;
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> matrix(n, vector<int>(m, 0));
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                cin >> matrix[i][j];
+        Solution obj;
+        int ans = obj.findCoverage(matrix);
+        cout << ans << "\n";
+    }
+    return 0;
 }
 // } Driver Code Ends
