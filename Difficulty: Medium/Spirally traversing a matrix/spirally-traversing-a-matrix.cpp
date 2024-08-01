210c62c38fa59e8f495c4a2c1b84e2df1f033fe1
Smit R. PATEL <smitrpatel19@gnu.ac.in>
2024-08-01 19:07:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Spirally traversing a matrix/spirally-traversing-a-matrix.cpp
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    
+    vector<int> spirallyTraverse(vector<vector<int> > &matrix) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+    
+        vector<int> ans;
+        
+        int left=0;
+        int right=m-1;
+        int top=0;
+        int bottom=n-1;
+        
+        while(left<=right && top<=bottom)
+        {
+            //left to right
+            for(int i=left;i<=right;i++)
+            {
+                ans.push_back(matrix[top][i]);
+            }
+            top++;
+            //top to bottom
+            
+            for(int i=top;i<=bottom;i++)
+            {
+                ans.push_back(matrix[i][right]);
+            }
+            right--;
+            
+            if(!(left<=right && top<=bottom))
+            break;
+            
+            //right to left
+            for(int i=right;i>=left;i--)
+            {
+                ans.push_back(matrix[bottom][i]);
+            }
+            bottom--;
+            
+            //bottom to top
+            for(int i=bottom;i>=top;i--)
+            {
+                ans.push_back(matrix[i][left]);
+            }
+            left++;
+            
+        }
+        return ans;
+    }
+
+
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int r, c;
+        cin >> r >> c;
+        vector<vector<int>> matrix(r, vector<int>(c, 0));
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                cin >> matrix[i][j];
+            }
+        }
+
+        Solution ob;
+        vector<int> result = ob.spirallyTraverse(matrix);
+        for (int i = 0; i < result.size(); ++i)
+            cout << result[i] << " ";
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
