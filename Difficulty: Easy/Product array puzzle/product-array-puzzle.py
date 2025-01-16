e75a98ac63fba1166a61643ac8ec40f2557be1e6
Smit R. PATEL <smitrpatel19@gnu.ac.in>
2025-01-17 02:37:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Product array puzzle/product-array-puzzle.py
+++ Difficulty: Easy/Product array puzzle/product-array-puzzle.py
@@ -1,34 +1,30 @@
 #User function Template for python3
 
 class Solution:
-    def productExceptSelf(self, nums):
+    def productExceptSelf(self, arr):
         #code here
-        p=1
-        zero=0
-        
-        for i in nums:
-            if i==0:
-                zero+=1
-                continue
-            p*=i
-        
-        for i in range(0,len(nums)):
-            
-            if zero==0:
-                
-                nums[i]= (p//nums[i])
-                
-            elif zero==1:
-                
-                if nums[i]==0:
-                    nums[i]=p
-                else:
-                    nums[i]=0
+        res=[]
+        s=1
+        a=1
+        if arr.count(0)>1:  #if more than 0 than multiplication will be zero
+            res=[0]*len(arr)
+            return res
+        for i in arr:
+            s*=i
+            if i != 0:  #storing if one number is 0
+                a*=i
+        for i in arr:
+            #print(s)
+            if s == 0 and i == 0: 
+                    res.append(a)
+            elif s==0 and i!=0:
+                res.append(0)
             else:
-                
-                nums[i]=0
-                
-        return nums
+                res.append(s//i)
+        return res
+
+
+
 
 #{ 
  # Driver Code Starts
@@ -38,10 +34,11 @@ if __name__ == '__main__':
     t = int(input())
 
     for _ in range(t):
-        n = int(input())
+
         arr = [int(x) for x in input().split()]
 
         ans = Solution().productExceptSelf(arr)
         print(*ans)
+        print("~")
 
 # } Driver Code Ends
