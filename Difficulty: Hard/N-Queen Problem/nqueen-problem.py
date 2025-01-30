ed0e4b31fb3b5eb07a5fb1ee7552a9c6e69b4661
Smit R. PATEL <smitrpatel19@gnu.ac.in>
2025-01-30 09:57:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/N-Queen Problem/nqueen-problem.py
+++ Difficulty: Hard/N-Queen Problem/nqueen-problem.py
@@ -1,18 +1,32 @@
 #User function Template for python3
 
+
 class Solution:
-    def solve(self, col, board, n, sol):
-        if col == n:
-            sol.append(board[:])
-        for row in range(1, n + 1):
-            if all(board[i] != row and abs(board[i] - row) != col - i for i in range(col)):
-                board[col] = row
-                self.solve(col + 1, board, n, sol)
-    
     def nQueen(self, n):
-        sol = []
-        self.solve(0, [0] * n, n, sol)
-        return sol
+        bd=[None]*n
+        rw=set()
+        up=set()
+        dn=set()
+        ret=[]
+        def bt(x=0):
+            nonlocal n,bd,rw,up,dn,ret
+            if x==n:
+                return True
+            for y in range(n):
+                if y in rw or y+x in up or y-x in dn:
+                    continue
+                bd[x]=y
+                rw.add(y)
+                up.add(y+x)
+                dn.add(y-x)
+                if bt(x+1):
+                    ret.append([x+1 for x in bd])
+                bd[x]=None
+                rw.discard(y)
+                up.discard(y+x)
+                dn.discard(y-x)
+        bt()
+        return ret
 
 
 
@@ -24,17 +38,20 @@ if __name__ == '__main__':
     t = int(input())
     for _ in range(t):
         n = int(input())
-        
+
         ob = Solution()
         ans = ob.nQueen(n)
-        if(len(ans) == 0):
+        if (len(ans) == 0):
             print("-1")
         else:
+            ans.sort()
             for i in range(len(ans)):
-                print("[",end="")
+                print("[", end="")
                 for j in range(len(ans[i])):
-                    print(ans[i][j],end = " ")
-                print("]",end = " ")
+                    print(ans[i][j], end=" ")
+                print("]", end=" ")
             print()
-                
+
+        print("~")
+
 # } Driver Code Ends
