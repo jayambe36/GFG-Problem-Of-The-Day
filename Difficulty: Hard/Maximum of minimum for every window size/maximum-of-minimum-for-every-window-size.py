dc026213d6f3e90c800e80333a86f50bd6006cac
Smit R. PATEL <smitrpatel19@gnu.ac.in>
2025-02-26 18:36:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Maximum of minimum for every window size/maximum-of-minimum-for-every-window-size.py
@@ -0,0 +1,66 @@
+
+class Solution:
+    def maxOfMins(self, arr):
+       # code here
+       n = len(arr)
+       nxs = self.next_smaller(arr)
+       pvs = self.previous_smaller(arr)
+       result = [float('-inf')]*n
+    
+       for i in range(n):
+           idx = nxs[i] - pvs[i] - 2
+           result[idx] = max(result[idx], arr[i])
+           
+       stack = []
+       
+       for i in reversed(range(n)):
+           
+           while stack and result[stack[-1]] < result[i]:
+               stack.pop()
+           
+           if stack: result[i] = max(result[i], result[stack[-1]])
+           stack.append(i)
+           
+       return result
+       
+    def next_smaller(self, arr):
+        n = len(arr)
+        nxs = [n]*n
+        stack = []
+        
+        for i in reversed(range(n)):
+            
+            while stack and arr[stack[-1]] >= arr[i]:
+                stack.pop()
+            
+            if stack: nxs[i] = stack[-1]
+            stack.append(i)
+        
+        return nxs
+    
+    def previous_smaller(self, arr):
+        n = len(arr)
+        pxs = [-1]*n
+        stack = []
+        
+        for i in range(n):
+            
+            while stack and arr[stack[-1]] >= arr[i]:
+                stack.pop()
+            
+            if stack: pxs[i] = stack[-1]
+            stack.append(i)
+        
+        return pxs
+
+#{ 
+ # Driver Code Starts
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        arr = list(map(int, input().split()))
+        solution = Solution()
+        result = solution.maxOfMins(arr)
+        print(" ".join(map(str, result)))
+        print("~")
+# } Driver Code Ends
