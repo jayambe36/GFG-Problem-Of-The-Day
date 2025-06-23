8a0f3b98e9d2a4d510a533863818a81f869e83b1
Smit R. PATEL <smitrpatel19@gnu.ac.in>
2025-06-23 10:48:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum sum/minimum-sum.cpp
+++ Difficulty: Medium/Minimum sum/minimum-sum.cpp
@@ -1,66 +1,35 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function template for C++
-
-
 class Solution {
   public:
+    string addstring(string& num1,string& num2){
+        string res;
+        int carry=0;
+        int n=num1.size();
+        for(int i=n-1;i>=0;i--){
+            int sum=(num1[i]-'0'+num2[i]-'0');
+            res.push_back((sum+carry)%10+'0');
+            carry=(sum+carry)/10;
+        }
+        if(carry)
+         res.push_back('1');
+        while(res.back()=='0')
+         res.pop_back();
+        reverse(res.begin(),res.end());
+        return res;
+    }
     string minSum(vector<int> &arr) {
-        
-        sort(arr.begin(), arr.end()); 
-
-        int l = arr.size(), rem = 0;
-        string ans = "";
-    
-        for (int i = l - 1; i >= 0; i -= 2) {
-            if (i - 1 >= 0) {
-                rem += arr[i] + arr[i - 1];  
-            } else {
-                rem +=  arr[i];  
-            }
-
-            if (rem > 0) ans.push_back((rem % 10) + '0'); 
-            
-            rem /= 10; 
+        // code here
+        sort(arr.begin(),arr.end());
+        string num1="",num2="";
+        int n=arr.size();
+        if(n&1)
+         num2.push_back('0');
+        for(int i=0;i<n;i++){
+            if(i&1)
+             num2.push_back(arr[i]+'0');
+            else
+             num1.push_back(arr[i]+'0');
         }
-    
-        if (rem > 0) ans.push_back((rem % 10) + '0');
-    
-        reverse(ans.begin(), ans.end());
-        
-        return ans;
+        string res=addstring(num1,num2);
+        return res;
     }
-
 };
-
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> a;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            a.push_back(number);
-        }
-
-        Solution ob;
-        string ans = ob.minSum(a);
-        cout << ans << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
