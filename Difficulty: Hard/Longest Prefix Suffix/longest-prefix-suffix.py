ddb6d7c0b249bca17705ba0bc6272dedcfee6a6d
Smit R. PATEL <smitrpatel19@gnu.ac.in>
2025-08-08 06:03:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Longest Prefix Suffix/longest-prefix-suffix.py
+++ Difficulty: Hard/Longest Prefix Suffix/longest-prefix-suffix.py
@@ -1,40 +1,25 @@
-#User function Template for python3
-
 class Solution:
-	def lps(self, str):
+	def getLPSLength(self, s):
 		# code here
+		# ....(j-1)j .....(i-1)i ...
+		# lsp[i]: matching length end at index i
+		
 		n = len(s)
-        lps = [0] * n
-        length = 0
-        i = 1
-    
-        while i < n:
-            if s[i] == s[length]:
-                length += 1
-                lps[i] = length
-                i += 1
-            else:
-                if length != 0:
-                    length = lps[length - 1]
-                else:
-                    lps[i] = 0
-                    i += 1
- 
-        return lps[-1]
-
-
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-if __name__ == '__main__':
-    T = int(input())
-    for i in range(T):
-        s = input()
-
-        ob = Solution()
-        answer = ob.lps(s)
-        print(answer)
-
-# } Driver Code Ends
+		lsp = [0]*n
+		for i in range(1, n):
+		    # find the matched length of previous index i-1
+		    # then s[j] will be next character on the prefix
+		    j = lsp[i-1]
+		    # if the current i can't match the coming j (in the prefix)
+		    # since lsp[i-1] = j, the prefix with j characters should be
+		    # the same as j characters immediately before i. s[i-j:i]
+		    # it looks like ...([{}   {}]      [{}   {}])(j) ..............([{}     {}]    [{}    {}])(i).......
+		    # the length of () is j, all (), [], {} are equal respectively.
+		    #                                                                      
+		    while j > 0 and s[i] != s[j]:
+		        j = lsp[j-1]
+		    if s[i] == s[j]:
+		        j += 1
+		    lsp[i] = j
+        return lsp[-1]
+		
