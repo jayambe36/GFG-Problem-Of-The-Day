cccc6c2bb19dadfff551cf3cf541092d968e5039
Smit R. PATEL <smitrpatel19@gnu.ac.in>
2025-08-12 16:06:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Shop in Candy Store/shop-in-candy-store.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minMaxCandy(self, prices, k):
+        # code here
+        prices.sort()
+        if len(prices)/(k+1) == len(prices)//(k+1):
+            buy=len(prices)//(k+1)
+        else:
+            buy=(len(prices)//(k+1))+1
+        mini=sum(prices[:buy])
+        maxi=sum(prices[-buy:])
+        return [mini, maxi]
