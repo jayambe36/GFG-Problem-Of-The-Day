9a07d6caa3dc3d664d0a761c9894432c26c96e41
Smit R. PATEL <smitrpatel19@gnu.ac.in>
2025-08-18 15:12:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find H-Index/find-hindex.py
+++ Difficulty: Medium/Find H-Index/find-hindex.py
@@ -1,37 +1,9 @@
-#User function Tem
-class Solution:  
-    # Function to find hIndex
-    def hIndex(self, citations):
-        #code here
-        c=0
-        i=0
-        n=len(citations)
-        arr.sort()
-        arr.reverse()
-        for i in range(n):
-            if citations[i]>=i+1:
-                c+=1
-            else:
+class Solution:
+     def hIndex(self, citations):
+        citations.sort(reverse=True)
+        h = 0
+        for e in citations:
+            if e < h+1:
                 break
-        return c
-
-
-
-#{ 
- # Driver Code Starts
-# Initial Template for Python 3
-
-# Main
-if __name__ == '__main__':
-    t = int(input())
-    while t > 0:
-        t -= 1
-        arr = [int(x) for x in input().strip().split()]
-
-        ob = Solution()
-        result = ob.hIndex(arr)
-
-        print(result)
-        print("~")
-
-# } Driver Code Ends
+            h += 1
+        return h
