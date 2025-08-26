a1e088c46d14572d89997f34021c5938532d1278
Smit R. PATEL <smitrpatel19@gnu.ac.in>
2025-08-27 00:21:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count the number of possible triangles/count-the-number-of-possible-triangles.py
+++ Difficulty: Medium/Count the number of possible triangles/count-the-number-of-possible-triangles.py
@@ -1,7 +1,4 @@
-#User function Template for python3
-
 class Solution:
-    #Function to count the number of possible triangles.
     def countTriangles(self, arr):
         # code here
         arr.sort()
@@ -17,19 +14,3 @@ class Solution:
                 else:
                     i+=1
         return count
-
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-if __name__ == '__main__':
-    t = int(input())
-    for _ in range(t):
-        arr = list(map(int, input().strip().split()))
-        ob = Solution()
-        print(ob.countTriangles(arr))
-
-        print("~")
-
-# } Driver Code Ends
